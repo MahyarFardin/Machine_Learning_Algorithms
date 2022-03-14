1f497f3a7145d7ae80bc81b22655f0841eaa8f7f
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-14 20:34:59 +0330

    Done

--- KNieresNeghbors/KNN.py
+++ KNieresNeghbors/KNN.py
@@ -4,24 +4,40 @@ def Euclidean_distance(first_point, second_point): # This function will loop thr
         sum += (first_point[i]-second_point[i])**2
     return (sum)**0.5
 
-def Frequent(data):
-    return max(set(data), key = data.count)
+def Frequent(data): # This function will find the most frequent classes
+    freq=0
+    maximum=0
+    for i in data:
+        counter=0
+        for j in data:
+            if i==j:
+                counter += 1
+        if counter > maximum:
+            maximum=counter
+            freq = i 
 
+    return freq
 
-def predict(xtrain, ytrain, xtest):
+def predict(xtrain, ytrain, xtest, k):
     answere=[]
     for i in xtest:
         dist=[]
         for j in range(len(xtrain)):
-            dist.append([Euclidean_distance(i, xtrain[j]), ytest[i]]) # This is for calculating all of the distances
-        dist=sorted(dist)
-        answere.append(Frequent(dist))
+            dist.append([Euclidean_distance(i, xtrain[j]), ytrain[j]]) # This is for calculating all of the distances and appending them with their classes
+        
+        dist=sorted(dist) # Time stamp sort
+        
+        dist= dist[:k] # Just considering the k neighbors
+
+        labels=[x[1] for x in dist] # seperating labels from distances
+
+        answere.append(Frequent(labels))
 
     return answere
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
 
 iris=load_iris()
 
@@ -30,7 +46,7 @@ y=iris.target
 
 xtrain, xtest, ytrain, ytest=train_test_split(x,y)
 
-answere=predict(xtrain, ytrain, xtest)
-
+answere=predict(xtrain, ytrain, xtest, 5)
 
 print(confusion_matrix(answere, ytest))
+print(classification_report(answere, ytest))
