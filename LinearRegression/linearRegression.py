af8019cdfbe44850276b41b5785a076e744e269c
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-24 00:00:53 +0430

    done

--- LinearRegression/linearRegression.py
+++ LinearRegression/linearRegression.py
@@ -1,11 +1,57 @@
-class LinearRegression:
-    def __init__(self, lr, iterations):
-        self.lr=.001
-        self.iterations=1000
+import numpy as np
+
+class Linear_Regression:
+    def __init__(self, lr=.001, iterations=1000):
+        self.lr=lr
+        self.iterations=iterations
         self.weight=None
         self.bias=None
     
     def fit(self, x, y):
-        
+        sample_number, feature_number= x.shape
+
+        self.weight=np.zeros(feature_number)
+        self.bias=0
+
+        for _ in range(self.iterations):
+            predict = np.dot(x, self.weight)+self.bias
+
+            dw= (1/sample_number)*(np.dot(x.T, predict-y))
+            db= (1/sample_number)*(np.sum(predict-y))
+
+            self.weight -= self.lr*dw
+            self.bias -= self.lr*db
+
+
     def predict(self, x):
-        pass
+        prediction = np.dot(x, self.weight)+ self.bias
+        return prediction
+
+
+from sklearn import datasets
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+x, y= datasets.make_regression(n_samples=200, n_features=1)
+xtrain , xtest, ytrain, ytest= train_test_split(x, y)
+
+
+lr=Linear_Regression(lr=0.01)
+lr.fit(xtrain, ytrain)
+prediction_of_me=lr.predict(xtest)
+print(mean_squared_error(prediction_of_me, ytest))
+
+
+lr2=LinearRegression()
+lr2.fit(xtrain, ytrain)
+prediction_of_sklearn_lr=lr2.predict(xtest)
+print(mean_squared_error(prediction_of_sklearn_lr, ytest))
+
+plt.scatter(xtrain, ytrain, c="green")
+plt.scatter(xtest, ytest, c="blue")
+plt.plot(xtest, prediction_of_me, c="black")
+plt.plot(xtest, prediction_of_sklearn_lr, c="red")
+plt.legend(["train","test","my_pred","sklearn_pred"])
+plt.savefig("result.jpg")
--- LinearRegression/pytest.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-
-print(np.dot([1,2,3],[1,2,3]))
