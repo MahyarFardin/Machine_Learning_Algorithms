b7dd0abd98076c8c8f887c64d551752396f33cc3
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-23 23:36:50 +0430

    done

---
+++ LinearRegression/linearRegression.py
@@ -0,0 +1,11 @@
+class LinearRegression:
+    def __init__(self, lr, iterations):
+        self.lr=.001
+        self.iterations=1000
+        self.weight=None
+        self.bias=None
+    
+    def fit(self, x, y):
+        
+    def predict(self, x):
+        pass
---
+++ LinearRegression/pytest.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print(np.dot([1,2,3],[1,2,3]))
