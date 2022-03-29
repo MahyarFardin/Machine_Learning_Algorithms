1ca7d9b3d971966bcbc24abdcc4cb63ad2e15999
mahyarfardin <mahyarfardinfar@gmail.com>
2022-03-29 22:40:52 +0430

    Done

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,11 +1,10 @@
-import math
 import numpy as np
 
 class LogisticRegression:
-    def _sigmoid(self, x):
-        return 1/(1+ math.exp(x))
+    def _sigmoid(self, x):  # sigmoid function
+        return 1/(1+ np.power(2.71828,-x))
 
-    def __init__(self, lr=.1, n_iter=1000):
+    def __init__(self, lr=.1, n_iter=1000): # Constructor
 
         self.lr=lr
         self.n_iter=n_iter
@@ -17,12 +16,12 @@ class LogisticRegression:
         self.weights=np.zeros(n_features)
         self.bias=0
 
-        for _ in range(self.n_iter):
+        for _ in range(self.n_iter):    # Finding best power for the e in sigmoid using gradient descent
             model = np.dot(x, self.weights) + self.bias
             predict=self._sigmoid(model)
 
-            dw=1/(n_samples)*(np.dot(x.T, predict-y))
-            db=1/(n_samples)*(np.sum(predict-y))
+            dw=(1/n_samples)*(np.dot(x.T, (predict-y))) #Derivitive of weight
+            db=(1/n_samples)*(np.sum(predict-y)) #Derivitive of bias
 
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
@@ -31,22 +30,38 @@ class LogisticRegression:
         model = np.dot(x, self.weights) + self.bias
         predict=self._sigmoid(model)
 
-        predict=[1 if i > 0.5 else 0 for i in predict]
+        predict=[1 if i > 0.5 else 0 for i in predict]  # Applying the sigmoid part
 
         return predict
+
 
 
 # Test
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
 
 breast_canser = datasets.load_breast_cancer()
 x, y= breast_canser.data, breast_canser.target
 xtrain ,xtest, ytrain ,ytest= train_test_split(x, y)
 
+plt.scatter(x[5], y)
+plt.savefig("plot.jpg")
+
 lr=LogisticRegression()
 lr.fit(xtrain, ytrain)
 prediction= lr.predict(xtest)
 
-classification_report(prediction, ytest)    
+print(classification_report(prediction, ytest))  
+
+"""
+              precision    recall  f1-score   support
+
+           0       0.76      0.97      0.85        35
+           1       0.99      0.90      0.94       108
+
+    accuracy                           0.92       143
+   macro avg       0.87      0.93      0.90       143
+weighted avg       0.93      0.92      0.92       143
+"""
